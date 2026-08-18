6c3da4f6505da1371673d29bee95d108596f27ec
Pandemia404 <juliasheva84@gmail.com>
2026-08-18 08:37:57 +0300

    Remove PlaceId check from 4991214437.lua
    Removed version check for PlaceId in the script.

--- games/4991214437.lua
+++ games/4991214437.lua
@@ -1,7 +1,3 @@
---v1.5: +no scope sway when aiming
-
-if game.PlaceId ~= 4991214437 then return end
-
 local RunService = game:GetService("RunService")
 local Lighting = game:GetService("Lighting")
 local Terrain = workspace:FindFirstChildWhichIsA("Terrain")
